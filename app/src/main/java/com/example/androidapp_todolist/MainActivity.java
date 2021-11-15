1d861b6f2cc328e8dbc2a4632cb1a1544a737fc5
whitekf <90643199+whitekf@users.noreply.github.com>
2021-11-15 18:34:36 -0500

    Created MyListBasic Template w/ colors discussed and worked on button functionality

--- app/src/main/java/com/example/androidapp_todolist/MainActivity.java
+++ app/src/main/java/com/example/androidapp_todolist/MainActivity.java
@@ -6,8 +6,11 @@ import java.util.TimerTask;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class   MainActivity extends AppCompatActivity {
+    private Button button;
 
     Timer timer;
 
@@ -16,6 +19,7 @@ public class   MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // added below to allow 5 seconds for splashscreen timer
         timer = new Timer();
         timer.schedule(new TimerTask() {
             @Override
@@ -25,5 +29,20 @@ public class   MainActivity extends AppCompatActivity {
                 finish();
             }
         }, 5000);
+
+        // added below to add functionality to add button
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    // added below - this should open a new activity, but may need to correct name of activity (and should update method name)
+    public void openActivity2() {
+        Intent intent = new Intent(this, ListActivity.class);
+        startActivity(intent);
     }
 }

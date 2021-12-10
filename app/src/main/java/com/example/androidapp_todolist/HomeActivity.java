9a2e274bc93cc0699473e8b643358085cf233b3d
whitekf <90643199+whitekf@users.noreply.github.com>
2021-12-10 14:56:48 -0500

    Added functionality to Helpful Lists button to open new screen

--- app/src/main/java/com/example/androidapp_todolist/HomeActivity.java
+++ app/src/main/java/com/example/androidapp_todolist/HomeActivity.java
@@ -2,7 +2,10 @@ package com.example.androidapp_todolist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.opengl.GLDebugHelper;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
@@ -10,10 +13,34 @@ public class HomeActivity extends AppCompatActivity {
     // added button name for Helpful Lists (pre-filled lists for users to reference)
     Button helpfulLists;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        // helpful lists button (button1) functionality
+        helpfulLists = (Button) findViewById(R.id.button1);
+        helpfulLists.setOnClickListener(new View.OnClickListener() {
+
+            @Override  // I believe this determines 'what' (aka the text entry box) the "add" button click reacts to
+            public void onClick(View v) {
+
+                // Updated splashscreen to automatically go to screen with three buttons for the different list types
+                Intent intent = new Intent(HomeActivity.this, HelpfulLists.class);
+                startActivity(intent);
+                finish();
+                openHelpfulLists();
+            }
+
+        } );
+
+
     }
+
+    // added below - this should open a new activity, but may need to correct name of activity (and should update method name/origin of that name)
+    public void openHelpfulLists () {
+        Intent intent = new Intent(this, HelpfulLists.class);
+        startActivity(intent);
+    }
+
 }

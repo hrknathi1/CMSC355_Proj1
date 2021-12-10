b8ab2153e04dbbaf10925dce4572d282fbe2957a
whitekf <90643199+whitekf@users.noreply.github.com>
2021-12-10 14:30:01 -0500

    Adding new button to home activity for pre-built lists

--- app/src/main/java/com/example/androidapp_todolist/HomeActivity.java
+++ app/src/main/java/com/example/androidapp_todolist/HomeActivity.java
@@ -3,8 +3,13 @@ package com.example.androidapp_todolist;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
+
+    // added button name for Helpful Lists (pre-filled lists for users to reference)
+    Button helpfulLists;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

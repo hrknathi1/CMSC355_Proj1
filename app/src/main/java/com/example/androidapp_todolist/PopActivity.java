3d7b644a2d15e7ecf2f0be70f155b2119071ccd3
brianna224 <briannapatrick2020@gmail.com>
2021-12-10 16:36:48 -0500

    worked on displaying the pop-up and getting it to close when clicked on button

--- app/src/main/java/com/example/androidapp_todolist/PopActivity.java
+++ app/src/main/java/com/example/androidapp_todolist/PopActivity.java
@@ -4,12 +4,42 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Gravity;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
 
 public class PopActivity extends Activity {
+    Button close_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pop);
+
+        // to close out of pop-up
+        close_btn = (Button) findViewById(R.id.close_btn);
+        close_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+
+        int width = dm.widthPixels;
+        int height = dm.heightPixels;
+
+        getWindow().setLayout((int)(width*.8), (int)(height*.5));
+
+        WindowManager.LayoutParams param = getWindow().getAttributes();
+        param.gravity = Gravity.CENTER;
+        param.x = 0;
+        param.y = -20;
+
+        getWindow().setAttributes(param);
     }
 }

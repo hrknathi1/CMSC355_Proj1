523074b00fc57b9d9c65f492f907ff9bd49b3c63
whitekf <90643199+whitekf@users.noreply.github.com>
2021-12-10 23:01:26 -0500

    Added workout routine to helpful list

--- app/src/main/java/com/example/androidapp_todolist/CleaningLists.java
+++ app/src/main/java/com/example/androidapp_todolist/CleaningLists.java
@@ -10,7 +10,7 @@ import android.widget.TextView;
 
 public class CleaningLists extends AppCompatActivity {
 
-    CheckedTextView cleaningList;
+  //  CheckedTextView cleaningList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
